81a4b30e4beb8574e8c5374a12970a7be7846ac7
Gabriel K <gab270b@gmail.com>
2025-08-26 10:18:06 +0200

    ownership

---
+++ ownership/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn first_subword(mut s: String) -> String {
+    let index = match s.chars().skip(1).position(|c| c.is_uppercase() || c == '_') {
+        Some(idx) => idx + 1,
+        None => return s,
+    };
+    s.truncate(index);
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let s1 = "helloWorld";
+        let s2 = "snake_case";
+        let s3 = "CamelCase";
+        let s4 = "just";
+
+        println!("first_subword({}) = {}", s1, first_subword(s1.to_owned()));
+        println!("first_subword({}) = {}", s2, first_subword(s2.to_owned()));
+        println!("first_subword({}) = {}", s3, first_subword(s3.to_owned()));
+        println!("first_subword({}) = {}", s4, first_subword(s4.to_owned()));
+    }
+}
